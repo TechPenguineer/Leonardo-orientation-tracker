7bb5750eefe6561b9400c0ac09c885fdd1c034dd
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-02 11:11:26 -0400

    added install intructions and run bash

---
+++ README.md
@@ -0,0 +1,10 @@
+# How To Setup
+
+Step 1:
+
+ ```sudo apt install git && git clone https://github.com/TechPenguineer/Leonardo-orientation-tracker.git```
+<br>
+<br>
+Step 2:
+
+```sudo chmod a+rw /dev/ttyACM0 && platformio run```
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 #define onboard 13
+
 void setup() {
   pinMode(onboard,OUTPUT);
 }
 
 void loop() 
 {
+  int i;
+  for(i=1; i<0; i++)
+  {
+    Serial.println(i);
+  }
   digitalWrite(onboard, LOW);
   delay(1000);
   digitalWrite(onboard, HIGH);
