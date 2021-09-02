bcae7f135ebbd33ae8f7f75f8f6a8d72b0f6d222
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-02 11:00:20 -0400

    connect led

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
-
+#define onboard 13
 void setup() {
   // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+
+    Serial.print("E");
+
 }
